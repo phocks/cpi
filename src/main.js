ca59b628c643f567562d8991a2ba8151957ec483
Joshua Byrd <phocks@gmail.com>
2018-07-04 11:08:08 +1000

    Rolling everything up

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import cpi from "./cpi.js";
 
 import dayjs from "dayjs";
 
-// dayjs("Sep-2017");
+console.log(cpi);
 
 console.log(dayjs(dayjs("Sep-2017").endOf("month")).format());
 
 export default function() {
-  console.log("Hello world!!!");
+  console.log("Hello world!!!!!");
 }
